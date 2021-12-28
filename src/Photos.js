fd12c2eb2d65be5b85e87aeeb897b6516c64ecc4
brittness <32600997+brittness@users.noreply.github.com>
2021-12-28 16:42:26 -0600

    image updates

--- src/Photos.js
+++ src/Photos.js
@@ -1,13 +1,26 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   console.log(props.photos);
   if (props.photos) {
     return (
       <section className="Photos">
-        {props.photos.map(function (photo, index) {
-          return <img src={photo.src.large} key={index} alt={index} />;
-        })}
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.large}
+                    className="img-fluid"
+                    alt={index}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {
